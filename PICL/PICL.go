1093cb4f1a1037d76f6076e84f32c6e8ead37490
Tobias Schaer <toby.schaer@gmail.com>
2017-02-18 22:57:44 +0100

    Basic driver file and compiler stub
    A runnable command from command line

---
+++ PICL/PICL.go
@@ -0,0 +1,16 @@
+/*
+PICL.go: The PICL parser & code generator
+*/
+
+package PICL
+
+import (
+	"bufio"
+)
+
+var Dump bool
+
+func Compile(reader *bufio.Reader) {
+	
+}
+
---
+++ piclc/piclc.go
@@ -0,0 +1,49 @@
+/*
+Driver file for N. Wirth's PICL compiler
+
+Build with:
+C:\> go install picl/piclc
+Run:
+C:\Data\Personal\go\bin> piclc file.asm
+*/
+
+package main
+
+import (
+	"picl-go/PICL"
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+const Ver = "PICL compiler v0.1"
+
+func init() {
+	flag.BoolVar(&PICL.Dump, "d", false, "Dump listing output to console")
+}
+
+func main() {
+
+	fmt.Printf("%s\n\n", Ver)
+
+	// Handle args
+	flag.Parse()
+	// Exit on error
+	if !(len(flag.Args()) > 0) {
+		fmt.Printf("Usage: piclc <flags> sourcefile.asm\n")
+		flag.PrintDefaults()
+		return
+	}
+
+	// Compile source file
+	filename := flag.Arg(0)
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Printf("Gonna compile %s\n", filename)
+		PICL.Compile(bufio.NewReader(file))
+	}
+}
